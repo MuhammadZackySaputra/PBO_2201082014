0c1c7b74af516ded5ed15aa671010de8b022c63b
MuhammadZackySaputra_2201082014 <vedroaska1@gmail.com>
2023-06-13 14:35:41 +0700

    Tugas tanggal (04/04/23),(18/04/23),(16/05/23),(06/06/23)
    MUHAMMAD ZACKY SAPUTRA
    2201082014

---
+++ src/CommandLineExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author TUF GAMING
+ */
+public class CommandLineExample {
+    public static void main(String[] args){
+        
+        System.out.println("Number of argument = "+args.length);
+        System.out.println("first of argument = "+args[0]);
+    }
+}
